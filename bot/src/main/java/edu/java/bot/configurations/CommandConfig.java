987f39bd865e456020c2f2d7b077cf4b0954e1a1
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-02-21 12:39:08 +0300

    first commit

---
+++ bot/src/main/java/edu/java/bot/configurations/CommandConfig.java
@@ -0,0 +1,36 @@
+package edu.java.bot.configurations;
+
+//import edu.java.bot.commands.CommandHelp;
+//import edu.java.bot.commands.CommandList;
+
+import edu.java.bot.commands.*;
+import edu.java.bot.processors.CommandHandler;
+import edu.java.bot.repository.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+import java.util.Map;
+
+
+@Configuration
+public class CommandConfig {
+    @Bean
+    CommandHandler commandHandler(UserService userService) {
+        var start = new CommandStart(userService);
+        var list = new CommandList(userService);
+        var track = new CommandTrack(userService);
+        var unTrack = new CommandUntrack(userService);
+
+        var help = new CommandHelp(List.of(start, list, track, unTrack));
+
+        return new CommandHandler(
+            Map.of(
+                "/start", start,
+                "/help", help,
+                "/list", list,
+                "/track", track,
+                "/untrack", unTrack
+            )
+        );
+    }
+}
