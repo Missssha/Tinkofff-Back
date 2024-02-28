3b91fc6bfcecd6947fc1eb7cce08f4a687dbfbe6
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-02-28 19:17:05 +0300

    проверил на checkstyle

--- scrapper/src/main/java/edu/java/client/GitHubClient.java
+++ scrapper/src/main/java/edu/java/client/GitHubClient.java
@@ -1,27 +1,23 @@
 package edu.java.client;
 
 import edu.java.dto.GitHubRepository;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class GitHubClient {
-    private WebClient webClient;
-    private static final String GITHUB = "https://api.github.com/";
+    private final WebClient webClient;
 
     public GitHubClient(WebClient webClient) {
+        this.webClient = webClient;
     }
 
-    @Bean
-    public WebClient gitHubClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder.baseUrl(GITHUB)
-            .defaultHeaders(h -> h.setBearerAuth(System.getenv("GITHUB_API_TOKEN_SECOND")))
-            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE).build();
-    }
-
-    public Mono<GitHubRepository> getRepositoryInfo(String name, String reposName) {
+    @GetMapping("/repos/{name}/{reposName}")
+    public Mono<GitHubRepository> getRepositoryInfo(
+        @PathVariable("name") String name,
+        @PathVariable("reposName") String reposName
+    ) {
         return webClient
             .get()
             .uri(x -> x.path("/repos/{name}/{reposName}")
