26b98e583b4aa8039b50bee76d519e4cf7cf9dfe
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-02-28 19:17:05 +0300

    проверил на checkstyle

--- scrapper/src/main/java/edu/java/configuration/WebClientConfig.java
+++ scrapper/src/main/java/edu/java/configuration/WebClientConfig.java
@@ -1,24 +1,22 @@
 package edu.java.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Configuration
-public class WebClientConfig {
+@Configuration public class WebClientConfig {
     private static final String GITHUB = "https://api.github.com/";
     private static final String STACKOVERFLOW = "https://api.stackexchange.com/2.3";
 
-
-    public WebClient gitHubClient(WebClient.Builder webClientBuilder) {
+    @Bean public WebClient gitHubClient(WebClient.Builder webClientBuilder) {
         return webClientBuilder.baseUrl(GITHUB)
             .defaultHeaders(h -> h.setBearerAuth(System.getenv("GITHUB_API_TOKEN_SECOND")))
             .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE).build();
     }
 
-
-    public WebClient stackOverFlowClient(WebClient.Builder webClientBuilder) {
+    @Bean public WebClient stackOverFlowClient(WebClient.Builder webClientBuilder) {
         return webClientBuilder.baseUrl(STACKOVERFLOW)
             .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE).build();
     }
