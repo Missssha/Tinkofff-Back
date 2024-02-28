f8223f40edabc75b559d4f143c5b6aa6c919ec4b
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-02-28 19:17:05 +0300

    проверил на checkstyle

--- scrapper/src/main/java/edu/java/dto/GitHubRepository.java
+++ scrapper/src/main/java/edu/java/dto/GitHubRepository.java
@@ -1,10 +1,22 @@
 package edu.java.dto;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class GitHubRepository {
+    @JsonProperty("id")
     private Long id;
+
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("defaultBranch")
     private String defaultBranch;
 }
