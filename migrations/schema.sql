4232fede3dc3000b2bcd86cda65c9a6b6e3e478c
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-03-11 21:30:34 +0300

    changing created tables

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,23 +1,27 @@
 CREATE TABLE chat
 (
-    id              bigint                  NOT NULL PRIMARY KEY,
-    chat_id         bigint                  NOT NULL,
-    link_id         bigint                  NOT NULL REFERENCES link(id)
+    id              bigint                  NOT NULL PRIMARY KEY
 );
 /**/
 CREATE TABLE link
 (
     id              bigint generated always as identity,
     url             text                     not null,
-    link_id         bigint                   not null,
-    chat_id         bigint                   NOT NULL REFERENCES chat (id),
-
     last_check_time timestamp with time zone not null,
-
     created_at      timestamp with time zone not null,
     created_by      text                     not null,
 
 
     primary key (id),
     unique (url)
+);
+/**/
+CREATE TABLE chat_with_link
+(
+    id              bigint                  NOT NULL PRIMARY KEY,
+    chat_id         bigint,
+    link_id         bigint,
+
+    FOREIGN KEY (chat_id)                   REFERENCES chat(id),
+    FOREIGN KEY (link_id)                   REFERENCES link(id)
 );
