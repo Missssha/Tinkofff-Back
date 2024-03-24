34571ee38a6fc4d8d8c40cf0c0ff91c3d11d25a7
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-03-24 16:19:27 +0300

    some fix test

--- bot/src/test/java/edu/java/bot/services/RestAPIServiceTest.java
+++ bot/src/test/java/edu/java/bot/services/RestAPIServiceTest.java
@@ -1,21 +1,13 @@
 package edu.java.bot.services;
 
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.request.SendMessage;
-import edu.java.bot.models.SessionState;
-import edu.java.bot.users.User;
 import edu.java.bot.repository.UserService;
+import edu.java.bot.users.User;
+import java.net.URI;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import static net.bytebuddy.matcher.ElementMatchers.any;
-import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 public class RestAPIServiceTest {
     @Mock
@@ -33,7 +25,7 @@ public class RestAPIServiceTest {
     }
 
     @Test()
-    public void testSendNotification_InvalidUri() throws Exception {
+    public void testSendNotificationInvalidUri() throws Exception {
         Long chatId = 12345L;
         String invalidUri = "invalid_uri_string";
         String description = "Test notification description";
