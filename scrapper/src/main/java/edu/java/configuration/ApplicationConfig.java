a6bba638fb75e5926dd46c636f2bab76352a8362
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-03-24 23:24:52 +0300

    some fix

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -29,7 +29,9 @@ public record ApplicationConfig(
     @NotEmpty
     String gitUrl,
     @NotEmpty
-    String stackUrl) {
+    String stackUrl,
+    @NotNull
+    AccessType databaseAccessType) {
     public record Scheduler(boolean enable,
                             @NotNull Duration interval,
                             @NotNull Duration forceCheckDelay) {
