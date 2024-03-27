83f748eee071b91a60f30928596056eda1b8dd54
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-03-27 20:09:42 +0300

    fix build (8)

--- scrapper/src/test/java/edu/java/scrapper/repository/JdbcChatRepositoryTest.java
+++ scrapper/src/test/java/edu/java/scrapper/repository/JdbcChatRepositoryTest.java
@@ -3,11 +3,14 @@ package edu.java.scrapper.repository;
 import edu.java.dto.Chat;
 import edu.java.repository.jdbc.JdbcChatRepository;
 import edu.java.scrapper.IntegrationTest;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
 import java.util.List;
+import liquibase.database.Database;
+import liquibase.exception.LiquibaseException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -17,27 +20,22 @@ import static org.junit.Assert.assertEquals;
 public class JdbcChatRepositoryTest extends IntegrationTest {
     @Autowired
     private JdbcChatRepository chatRepository;
-    @Autowired
-    private JdbcClient jdbcClient;
 
 
     @Test
     @Transactional
     @Rollback
-    public void testAdding() {
+    public void testAdding() throws SQLException, FileNotFoundException, LiquibaseException {
+        Database database = IntegrationTest.runMigrations(POSTGRES);
+        assertThat(database).isNotNull();
 
-        Chat chat = new Chat(123456789L);
-        assertThat(POSTGRES.isRunning()).isTrue();
-
+        Chat chat = new Chat();
+        chat.setChatId(1L);
         chatRepository.add(chat);
+        List<Chat> chats = chatRepository.findAll();
 
-        // Assert
-        List<Chat> chats =
-            jdbcClient.sql("SELECT * FROM chat WHERE chat_id = ?")
-                .param(1, chat.getChatId())
-                .query(Chat.class)
-                .list();
-        assertThat(chats.getFirst().getChatId()).isEqualTo(chat.getChatId());
+        assertEquals(1L, chats.get(0).getChatId());
+        chatRepository.remove(1L);
     }
 
 }
