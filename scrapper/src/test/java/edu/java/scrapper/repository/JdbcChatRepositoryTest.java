03fe46fd227945591ab51a1536090877e5f033af
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-03-27 20:03:34 +0300

    fix build (8)

--- scrapper/src/test/java/edu/java/scrapper/repository/JdbcChatRepositoryTest.java
+++ scrapper/src/test/java/edu/java/scrapper/repository/JdbcChatRepositoryTest.java
@@ -7,27 +7,37 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 public class JdbcChatRepositoryTest extends IntegrationTest {
     @Autowired
     private JdbcChatRepository chatRepository;
+    @Autowired
+    private JdbcClient jdbcClient;
 
 
     @Test
     @Transactional
     @Rollback
     public void testAdding() {
-        Chat chat = new Chat();
-        chat.setChatId(1L);
-        chatRepository.add(chat);
-        List<Chat> chats = chatRepository.findAll();
 
-        assertEquals(1L, chats.get(0).getChatId());
-        chatRepository.remove(1L);
+        Chat chat = new Chat(123456789L);
+        assertThat(POSTGRES.isRunning()).isTrue();
+
+        chatRepository.add(chat);
+
+        // Assert
+        List<Chat> chats =
+            jdbcClient.sql("SELECT * FROM chat WHERE chat_id = ?")
+                .param(1, chat.getChatId())
+                .query(Chat.class)
+                .list();
+        assertThat(chats.getFirst().getChatId()).isEqualTo(chat.getChatId());
     }
 
 }
