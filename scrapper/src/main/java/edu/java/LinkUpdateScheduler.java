db4e25ab737febe0c2eb16fa94bc097079da9147
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-03-30 13:01:22 +0300

    try comleted hw5_bonus

--- scrapper/src/main/java/edu/java/LinkUpdateScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdateScheduler.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import lombok.extern.log4j.Log4j2;
@@ -81,18 +82,32 @@ public class LinkUpdateScheduler {
     }
 
     private void updateStackOverFlowLink(Link link, Timestamp now) throws URISyntaxException {
-
-        String path = link.getUrl().getPath();
-        Pattern pattern = Pattern.compile("/questions/(?<id>\\d+)");
-        Matcher matcher = pattern.matcher(path);
-
-        StackOverFlowQuestion question =
-            stackOverFlowClient.fetchQuestion(Long.parseLong(matcher.group("id"))).block().getItems().get(0);
+        List<String> fragments = List.of(link.getUrl().toString().split("/"));
+        int idOfQuestion = Integer.parseInt(System.getenv("idQuestionName"));
+        StackOverFlowQuestion
+            question =
+            stackOverFlowClient.fetchQuestion(Long.parseLong(fragments.get(idOfQuestion))).getItems()
+                .getFirst();
         Timestamp lastActivity = question.getLastActivityAsTimestamp();
 
         if (lastActivity.after(link.getLastCheckTime())) {
-            botClient.updateLink(link.getUrl(), link.getChats());
+            String description = "обновление данных : ";
             jdbcLinkService.updateLinkLastCheckTime(link.getId(), now);
+
+            if (question.getAnswerCount() > jdbcLinkService.getLinkPropertiesById(link.getId()).getCountOfAnswer()) {
+                description += "\n"
+                    + "появился новый ответ";
+                jdbcLinkService.updateCountOfAnswersById(link.getId(), question.getAnswerCount());
+            }
+
+            if (question.getCommentCount() > jdbcLinkService.getLinkPropertiesById(link.getId()).getCountOfComments()) {
+                description += "\n"
+                    + "появился новый комментарий";
+                jdbcLinkService.updateCountOfCommentsById(link.getId(), question.getCommentCount());
+            }
+
+            botClient.updateLink(link.getUrl(), link.getChats());
+
         }
     }
 
@@ -117,4 +132,6 @@ public class LinkUpdateScheduler {
             return null;
         }
     }
+
+
 }
