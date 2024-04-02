cf5ba57228d2845e79a16300892dcded92d4b6e5
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-04-02 22:04:18 +0300

    Add delete-methods in repos

--- scrapper/src/main/java/edu/java/repository/jpa/JpaChatRepository.java
+++ scrapper/src/main/java/edu/java/repository/jpa/JpaChatRepository.java
@@ -3,6 +3,8 @@ package edu.java.repository.jpa;
 import edu.java.dto.Chat;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -11,7 +13,12 @@ public interface JpaChatRepository extends JpaRepository<Chat, Long> {
 
     void removeById(Long chatId);
 
+    @Modifying
+    @Query(value = "delete from chat_link where chat_id = ?1",
+           nativeQuery = true)
+    void removeChatInChatsLinks(Long id);
+
     List<Chat> findAll();
 
-    Chat  findChatById(Long id);
+    Chat findChatById(Long id);
 }
--- scrapper/src/main/java/edu/java/repository/jpa/JpaLinkRepository.java
+++ scrapper/src/main/java/edu/java/repository/jpa/JpaLinkRepository.java
@@ -1,7 +1,6 @@
 package edu.java.repository.jpa;
 
 import edu.java.dto.Link;
-import java.net.URI;
 import java.sql.Timestamp;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,13 +13,20 @@ public interface JpaLinkRepository extends JpaRepository<Link, Long> {
     List<Link> findAll();
 
     @Modifying
-    @Query("SELECT l FROM Link l WHERE (CURRENT_TIMESTAMP() - l.lastCheckTime) > 30000" + "ORDER BY l.lastCheckTime ASC LIMIT 100")
+    @Query("SELECT l FROM Link l WHERE (CURRENT_TIMESTAMP() - l.lastCheckTime) > 30000"
+        + "ORDER BY l.lastCheckTime ASC LIMIT 100")
     List<Link> findOldLinks();
 
     Link save(Link link);
+
     Link findLinkById(Long id);
 
     void removeLinkById(Long id);
+
+    @Modifying
+    @Query(value = "delete from chat_link where link_id = ?1",
+           nativeQuery = true)
+    void removeLinksInChatsLinks(Long id);
 
     @Modifying
     @Query("update Link set  lastCheckTime = :lastCheckTime where id = :linkId")
