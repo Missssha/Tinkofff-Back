5bc735647272ace57fd3d3ee70da350da471d33b
Missssha <mikhail.plyusnin.2003@mail.ru>
2024-04-15 21:33:52 +0300

    some fix

--- dto/src/main/java/request/LinkUpdateRequest.java
+++ dto/src/main/java/request/LinkUpdateRequest.java
@@ -6,10 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@RequiredArgsConstructor
 public class LinkUpdateRequest {
     @JsonProperty("id")
     private Long id;
